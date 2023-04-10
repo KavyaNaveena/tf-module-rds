96e1e78031a9a504affd48b7b8fbd2677dc483dc
Kavya Naveena <karumurikavya@gmail.com>
2023-04-10 16:48:56 +0530

    update

--- main.tf
+++ main.tf
@@ -16,9 +16,9 @@ resource "aws_rds_cluster" "main" {
 
 resource "aws_rds_cluster_instance" "main" {
   count              = var.no_of_instances
-  identifier         = "${var.env}-${count.index}"
+  identifier         = "${var.env}-rds-${count.index}"
   cluster_identifier = aws_rds_cluster.main.id
-  instance_class     = "var.instance_class"
+  instance_class     = var.instance_class
 }
 
 resource "aws_db_subnet_group" "main" {
