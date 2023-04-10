080f864d636567532e299c15958b4a1b1fc942d4
Kavya Naveena <karumurikavya@gmail.com>
2023-04-10 15:34:48 +0530

    update

---
+++ main.tf
@@ -0,0 +1,24 @@
+resource "aws_rds_cluster" "main" {
+  cluster_identifier      = "${var.env}-rds"
+  engine                  = "aurora-mysql"
+  engine_version          = "5.7.mysql_aurora.2.03.2"
+  database_name           =  var.database_name
+  master_username         = data.aws_ssm_parameter.user.value
+  master_password         = data.aws_ssm_parameter.pass.value
+  backup_retention_period = var.backup_retention_period
+  preferred_backup_window = var.preferred_backup_window
+  db_subnet_group_name    = aws_db_subnet_group.main.name
+
+  tags = merge(
+    var.tags,
+    { Name = "${var.env}-rds"} )
+}
+
+resource "aws_db_subnet_group" "main" {
+  name       = "${var.env}-rds"
+  subnet_ids = var.subnet_ids
+
+  tags = merge(
+    var.tags,
+    { Name = "${var.env}-subnet-group"} )
+}
